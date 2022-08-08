90897702965b394958e15cdb2016970791e41c59
Tyler Sustare <10850753+TylerSustare@users.noreply.github.com>
2022-08-07 23:01:26 -0700

    Add tests for counter endpoints

--- api/test/test_definitions.dart
+++ api/test/test_definitions.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:http/http.dart';
 import 'package:test/test.dart';
 
 void runTests(
   void Function(String name, Future<void> Function(String host)) testServer,
 ) {
+  Directory('${Directory.current.path}/bin/db').delete(recursive: true);
+
   testServer('root', (host) async {
     final response = await get(Uri.parse(host));
     expect(response.statusCode, 200);
@@ -35,5 +41,33 @@ void runTests(
     // https://github.com/dart-lang/shelf_static/issues/53 - should 405
     expect(response.statusCode, 200);
     expect(response.body, contains('pkg:shelf example'));
+  });
+
+  group('count', () {
+    testServer('get', (host) async {
+      final response = await get(Uri.parse('$host/count'));
+      expect(response.statusCode, 200);
+      var data = jsonDecode(response.body);
+      expect(data['count'], 0);
+    });
+
+    testServer('increment', (host) async {
+      final response = await get(Uri.parse('$host/count'));
+      var data = jsonDecode(response.body);
+      expect(data['count'], 0);
+
+      final randomNumber = Random().nextInt(1000);
+      final res = await post(
+        Uri.parse('$host/count'),
+        body: jsonEncode(
+          {'count': randomNumber},
+        ),
+      );
+      expect(res.statusCode, 200);
+
+      final r = await get(Uri.parse('$host/count'));
+      var count = jsonDecode(r.body);
+      expect(count['count'], randomNumber);
+    });
   });
 }
